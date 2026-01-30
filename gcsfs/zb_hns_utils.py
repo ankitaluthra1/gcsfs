ceb992fa8e2b977f5ac6bfa958811185f94fbdd7
suni72 <sunidhichandra@google.com>
2026-01-30 14:09:02 +0000

    Update imports for python sdk Update grpc_client since MRD and AAOW now accept AsyncGrpcClient object

--- gcsfs/zb_hns_utils.py
+++ gcsfs/zb_hns_utils.py
@@ -1,7 +1,7 @@
 import logging
 from io import BytesIO
 
-from google.cloud.storage._experimental.asyncio.async_appendable_object_writer import (
+from google.cloud.storage.asyncio.async_appendable_object_writer import (
     _DEFAULT_FLUSH_INTERVAL_BYTES,
     AsyncAppendableObjectWriter,
 )
