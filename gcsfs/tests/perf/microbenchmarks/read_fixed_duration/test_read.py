cfb0a0859a29b9263a219f3426fe250cdd31a078
Margubur Rahman <margubur@google.com>
2026-03-17 18:05:28 +0000

    test-readahead

--- gcsfs/tests/perf/microbenchmarks/read_fixed_duration/test_read.py
+++ gcsfs/tests/perf/microbenchmarks/read_fixed_duration/test_read.py
@@ -24,7 +24,7 @@ def _read_op_seq(gcs, file_paths, chunk_size, runtime):
     files_it = itertools.cycle(file_paths)
     while time.perf_counter() - start_time < runtime:
         path = next(files_it)
-        with gcs.open(path, "rb") as f:
+        with gcs.open(path, "rb", cache_type="readahead") as f:
             while time.perf_counter() - start_time < runtime:
                 data = f.read(chunk_size)
                 if not data:
